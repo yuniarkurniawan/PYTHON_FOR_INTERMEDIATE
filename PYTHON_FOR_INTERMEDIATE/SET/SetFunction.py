d10e5aa1e1d8409b648b509d53ccdf37917e1a9c
Yuniar Kurniawan Suroso <35044741+yuniarkurniawan@users.noreply.github.com>
2022-11-30 10:22:49 +0700

    Add files via upload

--- PYTHON_FOR_INTERMEDIATE/SET/SetFunction.py
+++ PYTHON_FOR_INTERMEDIATE/SET/SetFunction.py
@@ -20,3 +20,24 @@ class SetFunction:
         set_union = self.data_set_bilangan_satu.\
             union(self.data_set_bilangan_dua)
         return set_union
+
+    # fungsi difference digunakan untuk mencari elemen berbeda dari set
+    # sebelah kiri
+    def difference_function(self):
+        set_difference = self.data_set_bilangan_satu.\
+            difference(self.data_set_bilangan_dua)
+        return set_difference
+
+    # fungsi symmetric_difference digunakan untuk mencari elemen berbeda dari set
+    # sebelah kiri dan kanan
+    def symmetric_difference_function(self):
+        set_symmetrict_difference = self.data_set_bilangan_satu.\
+            symmetric_difference(self.data_set_bilangan_dua)
+        return set_symmetrict_difference
+
+    # fungsi isdisjoint akan mengembalikan nilai boolean. Jika terdapat satu
+    # kesamaan saja maka akan bernilai False
+    def isdisjoint_function(self):
+        set_isdisjoint = self.data_set_bilangan_satu.\
+            isdisjoint(self.data_set_bilangan_dua)
+        return set_isdisjoint
--- PYTHON_FOR_INTERMEDIATE/SET/SetFunctionTest.py
+++ PYTHON_FOR_INTERMEDIATE/SET/SetFunctionTest.py
@@ -54,6 +54,76 @@ class SetFunctionTest(unittest.TestCase):
         self.assertNotEqual(list_result, [4, 5, 6, 7])
     # ========== END UNION TEST
 
+    # ========== BEGIN DIFFERENCE TEST
+    def test_set_difference_success_scenario(self) -> None:
+        set_satu = {1, 2, 3}
+        set_dua = {3, 4, 5}
+        set_function = SetFunction(set_satu, set_dua)
+        result_difference = set_function.difference_function()
+
+        # set adalah unordered maka harus dirubah menjadi list terlebih dahulu
+        list_result = list(result_difference)
+        list_result.sort()
+        self.assertEqual(list_result, [1, 2])
+
+    def test_set_difference_fail_scenario(self) -> None:
+        set_satu = {1, 2, 3}
+        set_dua = {3, 4}
+        set_function = SetFunction(set_satu, set_dua)
+        result_difference = set_function.difference_function()
+
+        # set adalah unordered maka harus dirubah menjadi list terlebih dahulu
+        list_result = list(result_difference)
+        list_result.sort()
+        self.assertNotEqual(list_result, [4, 5])
+    # ========== END DIFFERENCE TEST
+
+    # ========== BEGIN SYMMETRICT_DIFFERENCE TEST
+    def test_set_symmetric_difference_success_scenario(self) -> None:
+        set_satu = {1, 2, 3}
+        set_dua = {3, 4, 5}
+        set_function = SetFunction(set_satu, set_dua)
+        result_symmetric_difference = set_function.\
+            symmetric_difference_function()
+
+        # set adalah unordered maka harus dirubah menjadi list terlebih dahulu
+        list_result = list(result_symmetric_difference)
+        list_result.sort()
+        self.assertEqual(list_result, [1, 2, 4, 5])
+
+    def test_set_symmetric_difference_fail_scenario(self) -> None:
+        set_satu = {1, 2, 3}
+        set_dua = {3, 4}
+        set_function = SetFunction(set_satu, set_dua)
+        result_symmetric_difference = set_function.\
+            symmetric_difference_function()
+
+        # set adalah unordered maka harus dirubah menjadi list terlebih dahulu
+        list_result = list(result_symmetric_difference)
+        list_result.sort()
+        self.assertNotEqual(list_result, [1, 4, 5])
+    # ========== END SYMMETRICT_DIFFERENCE TEST
+
+    # ========== BEGIN ISDISJOINT TEST
+    def test_set_isdisjoint_success_scenario(self) -> None:
+        set_satu = {1, 2}
+        set_dua = {3, 4, 5}
+        set_function = SetFunction(set_satu, set_dua)
+        result_isdisjoint = set_function.\
+            isdisjoint_function()
+
+        self.assertEqual(result_isdisjoint, True)
+
+    def test_set_isdisjoint_fail_scenario(self) -> None:
+        set_satu = {1, 2, 3}
+        set_dua = {3, 4}
+        set_function = SetFunction(set_satu, set_dua)
+        result_isdisjoint = set_function.\
+            isdisjoint_function()
+
+        self.assertNotEqual(result_isdisjoint, True)
+    # ========== END SYMMETRICT_DIFFERENCE TEST
+
 
 if __name__ == '__main__':
     unittest.main()
